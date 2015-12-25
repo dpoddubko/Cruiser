14d5f5c48704dc375efc8dea28c8e76c7e5e34d9
dpoddubko <dpoddubko@yandex.ru>
2015-12-25 10:21:12 +0200

    в SortTest добавил тест sortMaybeTrueDamageTest()

--- src/cruiser/SortTest.java
+++ src/cruiser/SortTest.java
@@ -1,15 +1,17 @@
 package cruiser;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
+
 import gun.BaseGun;
 import gun.BombGun;
 import gun.MachineGun;
 import gun.MissileSystem;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 
 
 public class SortTest {
@@ -18,17 +20,33 @@ public class SortTest {
     BaseGun bombGun = new BombGun();
     List<BaseGun> list = new ArrayList<BaseGun>();
 
-@Test
-   public void sortDamageTest(){
-    list.add(missileSystem);
-    list.add(machineGun);
-    list.add(bombGun);
-    int damage = 0;
-    Collections.sort(list);
-    for (BaseGun l : list) {
-        if (damage==0) damage = l.getDamage();
-        else
-            assertTrue(damage>l.getDamage());
+    @Test
+    public void sortDamageTest() {
+        list.add(missileSystem);
+        list.add(machineGun);
+        list.add(bombGun);
+        int damage = 0;
+        Collections.sort(list);
+        for (BaseGun l : list) {
+            if (damage == 0) damage = l.getDamage();
+            else
+                assertTrue(damage > l.getDamage());
+        }
     }
+
+    @Test
+    public void sortMaybeTrueDamageTest() {
+        list.add(missileSystem);
+        list.add(machineGun);
+        list.add(bombGun);
+        int damage=0;
+        Collections.sort(list);
+        for (BaseGun l : list) {
+            if (damage == 0) damage = l.getDamage();
+            else {
+                assertTrue(damage > l.getDamage());
+                damage = l.getDamage();
+            }
+        }
     }
 }
